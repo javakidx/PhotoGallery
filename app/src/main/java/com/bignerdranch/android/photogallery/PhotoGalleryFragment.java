b5e1093ee43f32b32533e656e8d20a6ae768af43
javakidx <javakidx@gmail.com>
2015-08-11 00:42:08 +0800

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.photogallery;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by bioyang on 15/8/11.
+ */
+public class PhotoGalleryFragment extends Fragment
+{
+}
